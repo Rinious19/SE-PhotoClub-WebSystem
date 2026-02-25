fe01da1e1fff2183f6c3f8b7dec48de0c174737f
Rin <kalinsuksaard@gmail.com>
2026-02-25 21:58:35 +0700

    downgrade eslint from 10.0.2 to 9.10.0 to fix dependency conflicts and additional setup file project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 1. นำเข้าโมดูล path
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 2. ตั้งค่า alias ให้ชี้ไปที่โฟลเดอร์ src ของเรา
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
